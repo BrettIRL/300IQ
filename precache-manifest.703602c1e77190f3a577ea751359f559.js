01fb6ff5ac4d5814083ee4cd0b1517421a037345
Brett Cocking <brett@trademarktechnology.co.za>
2019-04-14 13:18:08 +0200

    Updates

---
+++ precache-manifest.703602c1e77190f3a577ea751359f559.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5c14e8350393be4da5f",
+    "url": "/300IQ/static/js/runtime~main.8e58b525.js"
+  },
+  {
+    "revision": "fac51cb934fb4825f035",
+    "url": "/300IQ/static/js/main.1b9b27f4.chunk.js"
+  },
+  {
+    "revision": "102c4e2534990d4f9b81",
+    "url": "/300IQ/static/js/2.429ed41a.chunk.js"
+  },
+  {
+    "revision": "fac51cb934fb4825f035",
+    "url": "/300IQ/static/css/main.a878bdad.chunk.css"
+  },
+  {
+    "revision": "9cb8de37698f26fba068206c613b6ca0",
+    "url": "/300IQ/index.html"
+  }
+];
